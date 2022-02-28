a49abd74134025a4287b13beebc3cd851a99b2cb
TGathman <tgathman@gmail.com>
2022-02-27 21:44:01 -0600

    Initial commit

---
+++ 1. Two Sum/two_sum.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        '''
+        Original, naive solution
+        Runtime 5960 ms
+        '''
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        '''
+        Using a hash map.
+        Runtime 97 ms
+        '''
+
+        complements = {}
+
+        for idx, ele in enumerate(nums):
+            comp = target - ele
+            if comp in complements:
+                return [idx, complements[comp]]
+            complements[ele] = idx
